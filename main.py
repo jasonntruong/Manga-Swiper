bfc080402efb70c8534b732b5198175534db72f2
Jason Truong <71723813+jasonntruong@users.noreply.github.com>
2022-03-20 03:36:17 -0400

    final commit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
 import mediapipe
+from pynput.keyboard import Key, Controller
 
 pollCounter = 0
 oldcx = None
 oldcy = None
+
+keyb = Controller()
 
 capture = cv2.VideoCapture(0)
 
@@ -11,7 +14,7 @@ mediapipeHands = mediapipe.solutions.hands
 
 myHands = mediapipeHands.Hands(
     static_image_mode=False,
-    max_num_hands=2,
+    max_num_hands=1,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
@@ -25,25 +28,42 @@ while True:
 
     if foundHands.multi_hand_landmarks:
         for hands in foundHands.multi_hand_landmarks:
+            actionDone = False
             for id, location in enumerate(hands.landmark):
                 h, w, c = img.shape
                 cx = int(location.x * w)
                 cy = int(location.y * h)
-                cv2.circle(img, (cx, cy), 3, (255,0,255), cv2.FILLED)
 
-                if pollCounter == 200:
+                if pollCounter == 200: #compares user hand position right now vs 200 loops ago. gives time for the user to move
                     pollCounter = 0
-                    if oldcx and oldcy:
+                    if oldcx and oldcy and not actionDone:
+                        #Swipe up
                         if abs(oldcx - cx) < 100 and (oldcy - cy) > 100:
-                            print("SWIPE DETECTED")
+                            keyb.press(Key.space)
+                            keyb.release(Key.space)
+                            actionDone = True
+
+                        #Swipe left --> comp goes right
+                        elif (oldcx - cx) > 150 and abs(oldcy - cy) < 100:
+                            keyb.press(Key.right)
+                            keyb.release(Key.right)
+                            actionDone = True
+
+                        #Swipe right --> comp goes left
+                        elif (oldcx - cx) < -150 and abs(oldcy - cy) < 100:
+                            keyb.press(Key.left)
+                            keyb.release(Key.left)
+                            actionDone = True
                     
                     oldcx = cx
                     oldcy = cy
                 pollCounter += 1
-            drawHands.draw_landmarks(img, hands, mediapipeHands.HAND_CONNECTIONS)
 
-            cv2.putText(img,str("X: "+str(cx)+" Y: "+str(cy)), (10,70), cv2.QT_FONT_NORMAL, 1, (0,0,255), 1)
+            drawHands.draw_landmarks(img, hands, mediapipeHands.HAND_CONNECTIONS) #draw landmarks on hand
 
+            cv2.putText(img,str("X: "+str(cx)+" Y: "+str(cy)), (10,70), cv2.QT_FONT_NORMAL, 1, (0,0,255), 1) #displays x and y of hand
+    
+    cv2.imshow("MangaReader", img) #show camera
 
-    cv2.imshow("MangaReader", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
